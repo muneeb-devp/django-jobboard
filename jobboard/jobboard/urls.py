5a353bc90791ee8a9d8fac6dfa19ca7d1c5997ca
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-20 18:31:01 +0500

    Added type annotations

--- jobboard/jobboard/urls.py
+++ jobboard/jobboard/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from typing import List
+from django.urls import URLPattern
 
-urlpatterns = [
+urlpatterns: List[URLPattern] = [
     path('admin/', admin.site.urls),
 
     path('api/', include('api.urls')),
